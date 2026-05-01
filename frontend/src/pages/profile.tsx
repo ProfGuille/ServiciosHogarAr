b81c593696f65f49657d07a9792f9ffde7978d20
ProfGuille <profguille@gmail.com>
2026-04-30 23:16:16 -0300

    fix: card perfil estira al alto de columna derecha (flex-1)

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -62,8 +62,8 @@ export default function Profile() {
         
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Profile Info */}
-          <div className="lg:col-span-2">
-            <Card>
+          <div className="lg:col-span-2 flex flex-col">
+            <Card className="flex-1">
               <CardHeader>
                 <CardTitle className="flex items-center gap-2">
                   <User className="h-5 w-5" />
